2aab8465c9ca353efadc3360b61eed4e2a80289b
D-Jaramillo23 <d.jaramillo@duocuc.cl>
2025-10-22 18:18:07 -0300

    Parte 3 Crear RegistroScreen con componentes de material 3 - Mostrar errores con iserror y supportingText -Boton si todo es valido navegar a "resumen"

--- app/src/main/java/com/example/actividad_2_3_3/ui/screen/RegistroScreen.kt
+++ app/src/main/java/com/example/actividad_2_3_3/ui/screen/RegistroScreen.kt
@@ -1,3 +1,4 @@
+package com.example.actividad_2_3_3.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,7 +18,103 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.example.ejemplo3.viewmodel.UsuarioViewModel
+
+@Composable
+fun RegistroScreen(
+    navController: NavController,
+    viewModel: UsuarioViewModel
+) {
+    val estado by viewModel.estado.collectAsState()
+
+    column(
+        Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 12.dp)
+    ){
+        // Campo nombre
+        OutlinedTextField(
+            value = estado.nombre,
+            onValueChange = viewModel::onNombreChange,
+            label = { Text(text = "Nombre")},
+            isError = estado.errores.nombre != null,
+            supportingText = {
+                estado.errores.nombre?.let {
+                    Text(text= it, color = MaterialTheme.colorScheme.error)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        //Campo correo
+        OutlinedTextField(
+            value = estado.correo,
+            onValueChange = viewModel::onCorreoChange,
+            label = { Text(text = "Correo electrónico")},
+            isError = estado.errores.correo != null,
+            supportingText = {
+                estado.errores.correo?.let {
+                    Text(text = it, color = MaterialTheme.colorScheme.error)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Campo clave
+        OutlinedTextField(
+            value = estado.clave,
+            onValueChange = viewModel::onClaveChange,
+            label = { Text(text = "Contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            isError = estado.errores.clave != null,
+            supportingText = {
+                estado.errores.clave?.let {
+                    Text(text = it, color = MaterialTheme.colorScheme.error)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Camo dirección
+        OutlinedTextField(
+            value = estado.direccion,
+            onValueChange = viewModel::onDireccionChange,
+            label = { Text(text = "Dirección") },
+            isError = estado.errores.direcion != null,
+            supportingText = {
+                estado.errores.direccion?.let {
+                    Text( text = it, color = MaterialTheme.colorScheme.error)
+                }
+            },
+        )
+
+        //Checkbox: Aceptar términos
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = estado.aceptaTerminos,
+                onCheckedChange = viewModel::onAceptarTerminosChange
+            )
+            Spacer(Modifier.width(width = 8.dp))
+            Text( text = "Acepto los términos y condiciones")
+        }
+    }
+
+    // Botón: enviar
+    Button(
+        onClick = {
+            if (ViewModel.validarFormulario()) {
+                navController.navigate(route = "resumen")
+
+            }
+        },
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text( text = "Registrar")
+    }
+}
