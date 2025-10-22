c4bf7bd5ddc60e7c52b3e74680f0b85357125544
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-10-22 19:43:16 -0300

    Parte 2 - Lógica del view model LISTO
    nos dimos cuenta por los import de la guía que UsuarioUiState.kt se crea en model no en viewmodel.

--- app/src/main/java/com/example/actividad_2_3_3/viewmodel/UsuarioViewModel.kt
+++ app/src/main/java/com/example/actividad_2_3_3/viewmodel/UsuarioViewModel.kt
@@ -1,4 +1,6 @@
 // Parte 2
+// Implementar UsuarioViewModel usando MutableStateFlow para almacenar estado
+
 package com.example.actividad_2_3_3.viewmodel
 
 import androidx.lifecycle.ViewModel
@@ -7,6 +9,7 @@ import com.example.actividad_2_3_3.model.UsuarioUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlin.compareTo
 
 class UsuarioViewModel : ViewModel() {
 
@@ -18,6 +21,50 @@ class UsuarioViewModel : ViewModel() {
 
     // Actualiza el campo nombre y limpia su error
     fun OnNombreChange(valor: String) {
-        _estado.update { it.copy(nombre = valor, errores) }
+        _estado.update { it.copy(nombre = valor, errores = it.errores.copy(nombre = null)) }
+    }
+
+    // Actualiza el campo correo
+    fun onCorreoChange(valor: String) {
+        _estado.update { it.copy(correo = valor, errores = it.errores.copy(correo = null)) }
+    }
+
+    // Actualiza el campo clave
+    fun onClaveChange(valor: String) {
+        _estado.update { it.copy(clave = valor, errores = it.errores.copy(clave = null)) }
+    }
+
+    // Actualiza el campo dirección
+    fun onDireccionChange(valor: String) {
+        _estado.update { it.copy(direccion = valor, errores = it.errores.copy(direccion = null)) }
+    }
+
+    // Actualiza el checkbox de aceptación
+    fun onAceptarTerminosChange(valor: Boolean) {
+        _estado.update { it.copy(aceptaTerminos = valor) }
+    }
+
+    // Funciones de modificación y función validarFormulario que retorna boolean y actualice errores:
+
+    // Validación global del formulario
+    fun validarFormulario(): Boolean {
+        val estadoActual = _estado.value
+        val errores = UsuarioErrores(
+            nombre = if (estadoActual.nombre.isBlank()) "Campo obligatorio" else null,
+            correo = if (!estadoActual.correo.contains("@")) "Correo inválido" else null,
+            clave = if (estadoActual.clave.length < 6) "Debe tener al menos 6 caracteres" else null,
+            direccion = if (estadoActual.direccion.isBlank()) "Campo obligatorio" else null
+        )
+
+        val hayErrores = listOfNotNull(
+            errores.nombre,
+            errores.correo,
+            errores.clave,
+            errores.direccion
+        ).isNotEmpty()
+
+        _estado.update { it.copy(errores = errores) }
+
+        return !hayErrores
     }
 }
